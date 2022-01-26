6406f463b49861c81192eb7d9b71093f510024ac
Aikamoine <atte.laakso@helsinki.fi>
2022-01-26 10:31:01 +0200

    initial database schema

---
+++ schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role TEXT
+);
+
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    servings INTEGER,
+    time INTEGER,
+    visible INTEGER
+);
+
+CREATE TABLE ingredients (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE units (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE recipe_ingredients (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    ingredient_id INTEGER REFERENCES ingredients,
+    unit_id INTEGER REFERENCES units,
+    amount TEXT
+);
+
+CREATE TABLE recipe_steps (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    step TEXT,
+    number INTEGER
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    recipe_id INTEGER REFERENCES recipes,
+    stars INTEGER,
+    comment TEXT
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag TEXT
+);
